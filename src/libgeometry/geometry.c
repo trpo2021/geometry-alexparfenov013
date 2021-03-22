e21ae396959bc9ecdf7e5bd4f89a28070e2f176e
Alexey Parfenov <alexp-02@mail.ru>
2021-03-22 17:46:08 +0300

    Fixed some problems with code in geometry.c

--- src/libgeometry/geometry.c
+++ src/libgeometry/geometry.c
@@ -5,12 +5,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void circlevoid(float x, float y, float r);
+const double pi=3.14;
 
-void circle(float x, float y, float r)
+void circlevoid(float x, float y, float r)
 {
+
 double perimetr = 0;
 double area = 0;
 perimetr = 2 * pi * r;
 area = pi * r * r;
-printf("perimetr = %.4f\narea = %.4f \n", perimetr, area);
+printf("perimetr = %.4f\narea = %.4f \n circle coordinates x - %.4f \n y - %.4f \n", perimetr, area, x, y);
 }
