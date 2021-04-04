58df57af4561a8aa550300ed57f6926a19d81fa8
Alexey Parfenov <alexp-02@mail.ru>
2021-04-04 14:34:48 +0300

    Add some functions

--- src/libgeometry/geometry.h
+++ src/libgeometry/geometry.h
@@ -1,3 +1,9 @@
 #pragma once
 
 void circlevoid(float x, float y, float r);
+int checkingexceptionPosCircle(char* checkingstring, char* checkingwordcircle);
+int checkingexceptionCheckingOnBracket(char* checkingstring);
+int checkingexceptionCheckingOnTwoBrackets(char* checkingstring);
+int checkingexceptionCheckComma(char* checkingstring);
+int checkingexceptionCheckMoreOneComma(char* checkingstring);
+int checkingexceptionCheckingSomeSymbols(char* checkingstring);
