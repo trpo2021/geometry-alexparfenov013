ec9bc6123c699650cf82ef23b66dabe2fa95612b
Alexey Parfenov <alexp-02@mail.ru>
2021-04-16 18:10:11 +0300

    Add new function

--- src/libgeometry/fucntions.c
+++ src/libgeometry/fucntions.c
@@ -105,3 +105,128 @@ int checkingexceptionCheckingSomeSymbols(char* checkingstring)
     }
     return flag;
 }
+
+int checkingOtherExceptions(char* checkingstring)
+{
+    char str[100][100], pr[] = "( ) ,", checkingstringtemp[200];
+    char checkingwordcircle[100] = "circle";
+    char* tmp;
+    int i = 0, flag = 0;
+    strcpy(checkingstringtemp, checkingstring);
+    tmp = strtok(checkingstring, pr);
+    while (tmp != NULL) {
+        strncpy(str[i], tmp, 100);
+        tmp = strtok(NULL, pr);
+        i++;
+    }
+
+    ////////////////
+    char* postempcomma;
+    int poscomma;
+    postempcomma = strchr(checkingstringtemp, ',');
+    poscomma = postempcomma - checkingstringtemp + 1;
+    ///////////////////////////
+    int numberofelements = i;
+    ////////////////////////////////////////////////////////////
+    char* postempvaluey;
+    int positionvaluey;
+    postempvaluey = strstr(checkingstringtemp, str[2]);
+    positionvaluey = postempvaluey - checkingstringtemp + 1;
+    if (poscomma < positionvaluey) {
+        flag = 1;
+    }
+
+    //////////////////////////////////////////////
+    if (str[1][0] == '.') {
+        flag = 1;
+    }
+    if (flag == 0)
+        if (str[2][0] == '.') {
+            flag = 1;
+        }
+    if (flag == 0)
+        if (str[3][0] == '.') {
+            flag = 1;
+        }
+    ///////////////////////////////////////////
+    if (str[1][0] == '0' && str[1][1] == '0') {
+        flag = 1;
+    }
+
+    if (flag == 0) {
+        if (str[2][0] == '0' && str[2][1] == '0') {
+            flag = 1;
+        }
+    }
+    if (flag == 0) {
+        if (str[3][0] == '0' && str[3][1] == '0') {
+            flag = 1;
+        }
+    }
+    ///////////////////////////////////////////
+    char* tempforstrchecking;
+    int checkingpositiondot = 1;
+    int checkingStrLenx;
+    int checkingStrLeny;
+    int checkingStrLenr;
+    checkingStrLenx = strlen(str[1]);
+    checkingStrLeny = strlen(str[2]);
+    checkingStrLenr = strlen(str[3]);
+    if (strchr(str[1], '.') != NULL) {
+        tempforstrchecking = strchr(str[1], '.');
+        checkingpositiondot = tempforstrchecking - str[1] + 1;
+        if (checkingpositiondot == checkingStrLenx) {
+            flag = 1;
+        }
+    }
+    checkingpositiondot = 0;
+    if (flag == 0)
+        if (strchr(str[2], '.') != NULL) {
+            tempforstrchecking = strchr(str[2], '.');
+            checkingpositiondot = tempforstrchecking - str[2] + 1;
+            if (checkingpositiondot == checkingStrLeny) {
+                flag = 1;
+            }
+        }
+
+    checkingpositiondot = 0;
+    if (flag == 0)
+        if (strchr(str[3], '.') != NULL) {
+            tempforstrchecking = strchr(str[3], '.');
+            checkingpositiondot = tempforstrchecking - str[3] + 1;
+            if (checkingpositiondot == checkingStrLenr) {
+                flag = 1;
+            }
+        }
+
+    char templast = '~';
+    if (flag == 0)
+        for (char tempA = 'A'; tempA <= templast; tempA++) {
+            if (strchr(str[1], tempA) != NULL) {
+                flag = 1;
+            }
+        }
+    if (flag == 0)
+        for (char tempA = 'A'; tempA <= templast; tempA++) {
+            if (strchr(str[2], tempA) != NULL) {
+                flag = 1;
+            }
+        }
+    if (flag == 0)
+        for (char tempA = 'A'; tempA <= templast; tempA++) {
+            if (strchr(str[3], tempA) != NULL) {
+                flag = 1;
+            }
+        }
+    if (numberofelements > 4) {
+        printf("Error at column 15: unexpected token \n");
+        flag = 1;
+    }
+    if (numberofelements < 4) {
+        flag = 1;
+    }
+    if (strcmp(str[0], checkingwordcircle) != 0) {
+        flag = 1;
+    }
+    return flag;
+}
