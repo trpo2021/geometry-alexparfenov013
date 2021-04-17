969d4abf8dc5330c07b38cf20dfc22d5bac66d80
Alexey Parfenov <alexp-02@mail.ru>
2021-04-17 17:22:07 +0300

    Add tests for programm

---
+++ test/test.c
@@ -0,0 +1,174 @@
+#include "ctest.h"
+#include "libgeometry/geometry.h"
+
+CTEST(suite, system_testing)
+{
+    ASSERT_EQUAL(1 + 2, 3);
+}
+
+CTEST(name_check, incorrect_name)
+{
+    char checkingstring = {"circle(1* 2)"};
+
+    int real = checkingexceptionCheckingSomeSymbols(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle(1 2, 3)"};
+
+    int real = checkingexceptionCheckingSomeSymbols(checkingstring);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name)
+{
+    char checkingstring = {"circle(1, 2, 3)"};
+
+    int real = checkingexceptionCheckMoreOneComma(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle(1 2, 3)"};
+
+    int real = checkingexceptionCheckMoreOneComma(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name)
+{
+    char checkingstring = {"circle(1 2 3)"};
+
+    int real = checkingexceptionCheckComma(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle(1 2, 3)"};
+
+    int real = checkingexceptionCheckComma(checkingstring);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name)
+{
+    char checkingstring = {"circle((1 2, 3))"};
+
+    int real = checkingexceptionCheckingOnTwoBrackets(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle(1 2, 3)"};
+
+    int real = checkingexceptionCheckingOnTwoBrackets(checkingstring);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name)
+{
+    char checkingstring = {"circle 1 2, 3"};
+
+    int real = checkingexceptionCheckingOnBracket(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle (1 2, 3)"};
+
+    int real = checkingexceptionCheckingOnBracket(checkingstring);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name)
+{
+    char checkingstring = {"(1 2, 3) circle"}, checkingwordcircle = {"circle"};
+
+    int real = checkingexceptionCheckingOnBracket(
+            checkingstring, checkingwordcircle);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle (1 2, 3)"}, checkingwordcircle = {"circle"};
+
+    int real = checkingexceptionCheckingOnBracket(
+            checkingstring, checkingwordcircle);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name_1)
+{
+    char checkingstring = {"circle (1. 2, 3)"};
+
+    int real = checkingOtherExceptions(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, incorrect_name_2)
+{
+    char checkingstring = {"circle (. 2, 3)"};
+
+    int real = checkingOtherExceptions(checkingstring);
+
+    int exp = 1;
+
+    ASSERT_EQUAL(exp, real);
+}
+
+CTEST(name_check, correct_name)
+{
+    char checkingstring = {"circle 1 2, 3)"};
+
+    int real = checkingOtherExceptions(checkingstring);
+
+    int exp = 0;
+
+    ASSERT_EQUAL(exp, real);
+}
