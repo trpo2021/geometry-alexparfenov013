7d48d4ec75d1c873f2192cf8ed45bea9fc9db66f
Alexey Parfenov <alexp-02@mail.ru>
2021-04-18 19:33:42 +0300

    Some changes

--- src/libgeometry/geometry.h
+++ src/libgeometry/geometry.h
@@ -1,6 +1,5 @@
 #pragma once
 
-void circlevoid(float x, float y, float r);
 int checkingexceptionPosCircle(char* checkingstring, char* checkingwordcircle);
 int checkingexceptionCheckingOnBracket(char* checkingstring);
 int checkingexceptionCheckingOnTwoBrackets(char* checkingstring);
@@ -8,3 +7,4 @@ int checkingexceptionCheckComma(char* checkingstring);
 int checkingexceptionCheckMoreOneComma(char* checkingstring);
 int checkingexceptionCheckingSomeSymbols(char* checkingstring);
 int checkingOtherExceptions(char* checkingstring);
+void circlevoid(float x, float y, float r);
